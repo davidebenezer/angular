82407daf5a5a9f0ff6ae2d3e520e25bb44662a98
davidebenezer <c.davidebenezer@gmail.com>
2021-07-16 01:40:00 -0500

    ng if else

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -3,12 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: '<div class="container"><div class="row"><div class="col-xs-12"><h1>List of Servers' +
-    `<input type="text" class="form-control" (input)="onEnterServerName($event)">
+    `<!--<input type="text" class="form-control" (input)="onEnterServerName($event)">-->
     <input type="text" class="form-control" [(ngModel)]="serverName">
     <button class="btn btn-primary" [disabled]="!addServer" (click)="onCreateServer()">Add Server</button><br>
-     </h1><hr><p [innerText]="addServer"></p>
-     {{serverName}}<br>
-     {{serverCreationStatus}}
+     </h1><hr>
+     <!--<p [innerText]="addServer"></p>-->
+     <!--{{serverName}}-->
+     <br>
+     <!--{{serverCreationStatus}}-->
+     <p *ngIf="isServerCreated; else noServer">{{serverCreationStatus}}</p>
+     <ng-template #noServer>
+       <p>{{serverCreationStatus}}</p>
+     </ng-template>
      <div app-server></div><div app-server></div><hr> Testing multiline
 
 to see if it works in combination with single quotes. it does work :)</div></div></div>
@@ -19,6 +25,7 @@ export class ServersComponent implements OnInit {
   addServer = false;
   serverCreationStatus = 'No Server Created';
   serverName = '';
+  isServerCreated = false;
 
   constructor() {
     setTimeout(() => this.addServer = true, 2000);
@@ -28,7 +35,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(): string{
-    this.serverCreationStatus = 'Server Created Successfully';
+    this.serverCreationStatus = 'Server Created Successfully. ServerId: '+this.serverName;
+    this.isServerCreated = true;
     return this.serverCreationStatus;
   }
 
